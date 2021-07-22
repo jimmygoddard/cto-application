3ea914cce2bd3aa19a63e65e3496e0892534d763
jimmygoddard <jimmy.goddard@gmail.com>
2021-07-22 12:21:32 -0400

    Added controller test class to be filled out later

---
+++ src/test/kotlin/com/cogitocorp/service/file/controller/FilesControllerTest.kt
@@ -0,0 +1,34 @@
+package com.cogitocorp.service.file.controller
+
+import org.junit.jupiter.api.Test
+
+internal class FilesControllerTest {
+
+  @Test
+  fun createFile() {
+  }
+
+  @Test
+  fun getFiles() {
+  }
+
+  @Test
+  fun getFile() {
+  }
+
+  @Test
+  fun zipFile() {
+  }
+
+  @Test
+  fun zipFiles() {
+  }
+
+  @Test
+  fun getMostRecentFiles() {
+  }
+
+  @Test
+  fun zipMostRecentFiles() {
+  }
+}
